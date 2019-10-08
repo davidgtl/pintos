237716990b106d546a84d7e92e62f84c33786956
davidgtl <david.ghitulescu@gmail.com>
2019-10-08 22:01:34 +0000

    Vanilla

---
+++ misc/pintos.sh
@@ -0,0 +1,2 @@
+export PATH=/home/david/workspace/osd/pintos/src/utils:/home/david/workspace/osd/pintos/bochs-2.6.9/bin:$PATH
+export BXSHARE=/home/david/workspace/osd/pintos/bochs-2.6.9/share/bochs
